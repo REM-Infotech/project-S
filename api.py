ddb2018f35c444f7268e9ef72965b23ee141415e
Nicholas Silva <nicholasazs213@gmail.com>
2023-01-05 06:56:44 -0300

    att

--- api.py
+++ api.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import pandas as pd
 
 app = Flask (__name__)
 
 # construir funcionalidades
-
 @app.route('/')
 def homepage():
     return 'Hello World'
 
-# start api
-
-app.run(host='0.0.0.0', port=80, debug=True)
+app.run(host='0.0.0.0', port='80', debug=True)
